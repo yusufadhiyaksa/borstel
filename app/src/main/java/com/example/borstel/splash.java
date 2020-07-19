1596cb1c30fd7fc6de8af44224f33b6dacb164bd
yusufadhiyaksa <S.yusufadhiyaksa@gmail.com>
2020-07-19 16:16:21 +0700

    Mengubah Warna StatusBar, Membuat statusbar menjadi trasparent pada splashscreen

--- app/src/main/java/com/example/borstel/splash.java
+++ app/src/main/java/com/example/borstel/splash.java
@@ -3,6 +3,7 @@ package com.example.borstel;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class splash extends Activity {
 
@@ -10,6 +11,7 @@ public class splash extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         Thread thread = new Thread(){
             public void  run(){
                 try{
